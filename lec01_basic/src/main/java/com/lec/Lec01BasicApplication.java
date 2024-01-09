03b83d9fbb4cf4f84e68cbf88c6c01be7e7fbb55
wkdeh <wkdeh@MAUVE>
2024-01-09 10:13:43 +0900

    test

---
+++ lec01_basic/src/main/java/com/lec/Lec01BasicApplication.java
@@ -0,0 +1,13 @@
+package com.lec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec01BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec01BasicApplication.class, args);
+	}
+
+}
